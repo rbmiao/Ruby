ef952f8f28bcf819f165cbfd030b71482c70a536
rbmiao <rbmiao@gmail.com>
2016-02-16 21:33:49 -0500

    Add more Ruby programs

---
+++ unPackParameters.rb
@@ -0,0 +1,10 @@
+# ruby unpackParameters.rb first 2nd 3d
+# ruby unpackParameters.rb my first work
+#
+
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
